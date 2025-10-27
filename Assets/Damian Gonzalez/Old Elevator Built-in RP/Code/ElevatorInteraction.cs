a71e64ce061b39355f263bd6f27b0753879570d3
ivycase <case@berkeley.edu>
2025-10-27 12:11:12 -0700

    fix: DEMO DAY LIVE HOTFIX!!!!!

--- Assets/Damian Gonzalez/Old Elevator Built-in RP/Code/ElevatorInteraction.cs
+++ Assets/Damian Gonzalez/Old Elevator Built-in RP/Code/ElevatorInteraction.cs
@@ -2,39 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace OldElevator {
-    public class ElevatorInteraction : MonoBehaviour {
+namespace OldElevator
+{
+    public class ElevatorInteraction : MonoBehaviour
+    {
         public LayerMask layerMaskButtons;
         public float maxInteractionDistance;
         public UnityEngine.UI.Image interactIcon;
 
-        void Start() {
+        void Start()
+        {
             if (layerMaskButtons == 0) layerMaskButtons = LayerMask.GetMask("ElevatorInteraction");
         }
 
 
-        void Update() {
+        void Update()
+        {
             if (Physics.Raycast(
                 transform.position,
                 transform.forward,
                 out RaycastHit hit,
                 maxInteractionDistance,
                 layerMaskButtons
-            )) {
+            ))
+            {
                 InteractionIcon.inst.SetInteractable(true);
                 //player can interact with an elevator interactable object
-                if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Fire1")) {
-                    if (hit.collider.transform.TryGetComponent(out ElevatorButton btn)) {
+                if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Fire1"))
+                {
+                    if (hit.collider.transform.TryGetComponent(out ElevatorButton btn))
+                    {
                         //it is a button. Press it.
                         btn.Press();
-                    } else if (hit.collider.transform.TryGetComponent(out DoorTrigger door)) {
+                    }
+                    else if (hit.collider.transform.TryGetComponent(out DoorTrigger door))
+                    {
                         //it is a door. Open/Close it.
                         door.Toggle();
                     }
+                    else if (hit.collider.transform.TryGetComponent(out CodeButton code))
+                    {
+                        code.Press();
+                    }
                 }
-            } else {
-                //can't interact
-                InteractionIcon.inst.SetInteractable(false);
+                else
+                {
+                    //can't interact
+                    InteractionIcon.inst.SetInteractable(false);
+                }
             }
         }
     }
